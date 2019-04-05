a781a342249cac8fe321bdd07f4a8912fc466f33
Mateusz Nguyen Tien <mateusznguyentien@gmail.com>
2019-04-05 18:05:44 +0200

    Simplified code

--- sketch_vacume-gauge-basic/sketch_vacume-gauge-basic.ino
+++ sketch_vacume-gauge-basic/sketch_vacume-gauge-basic.ino
@@ -5,35 +5,46 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
+  /*
   int sensor1Value = analogRead(A0);
-  float voltage1 = sensor1Value * (5.0 / 1023.0);
-  
   int sensor2Value = analogRead(A1);
-  float voltage2 = sensor2Value * (5.0 / 1023.0);
-  
   int sensor3Value = analogRead(A2);
-  float voltage3 = sensor3Value * (5.0 / 1023.0);
-  
   int sensor4Value = analogRead(A3);
-  float voltage4 = sensor4Value * (5.0 / 1023.0);
+  */
+  int sensorvalue[] = {analogRead(A0),analogRead(A1),analogRead(A2),analogRead(A3)};
+  
+  float input = 4.75;
+  
+  float kPa[4] ;
+
+  for (int i=0;i<=4;i++){
+    kPa[i] = voutkPa(sensorvalue[i],input);
+  }
 
  
-  // print out the value you read:
-  //float kPa=(11.1111 * voltage - 51.1111);
+ Serial.print(kPa[0], 4);
+ Serial.print(" ");
+ Serial.print(kPa[1], 4);
+ Serial.print(" ");
+ Serial.print(kPa[2], 4);
+ Serial.print(" ");
+ Serial.println(kPa[3], 4);
  
-  float kPa1 = (((voltage1/4.75)-0.92)/0.018);
-  float kPa2 = (((voltage2/4.63)-0.92)/0.018);
-  float kPa3 = (((voltage3/4.63)-0.92)/0.018);
-  float kPa4 = (((voltage4/4.63)-0.92)/0.018);
+
   
- Serial.print(kPa1, 4);
- Serial.print(" ");
- Serial.print(kPa2, 4);
- Serial.print(" ");
- Serial.print(kPa3, 4);
- Serial.print(" ");
- Serial.println(kPa4, 4);
-  
+ 
   delay(100);
+}
+
+
+float voutkPa(float sensorvalue ,float input){
+  
+  float voltage = sensorvalue * (5.0 / 1023.0);
+  float kPa = (((voltage/4.75)-0.92)/0.018);
+
+  if(kPa>0){
+    kPa=0;
+  }
+  
+  return kPa;
 }
